e157a19e20876e0b2fef458e1d68bbc36c53ba53
Ryan Johnston <ryjo1026@gmail.com>
2019-05-19 17:36:20 -0400

    dotfiles included in * glob

--- zsh/preferences.zsh
+++ zsh/preferences.zsh
@@ -12,5 +12,6 @@ export LDFLAGS=-L/usr/local/opt/openssl/lib
 
 # ZSH options
 setopt auto_cd                      # cd by typing directory name if it's not a command
+setopt GLOB_DOTS                    # include dotfiles in * glob
 
 export ZSH_AUTOSUGGEST_USE_ASYNC=1  # speed up autosuggestions
