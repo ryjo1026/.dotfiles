3def390a1d82e52d317fee08eaec9b4cbea8fd7a
Ryan Johnston <ryjo1026@gmail.com>
2019-06-01 23:52:39 -0400

    Add cd as pushd

--- zsh/preferences.zsh
+++ zsh/preferences.zsh
@@ -13,5 +13,8 @@ export LDFLAGS=-L/usr/local/opt/openssl/lib
 # ZSH options
 setopt auto_cd                      # cd by typing directory name if it's not a command
 setopt GLOB_DOTS                    # include dotfiles in * glob
+# make cd behave like pushd (have a stack)
+setopt autopushd pushdminus pushdsilent pushdtohome cdablevars
+DIRSTACKSIZE=5
 
 export ZSH_AUTOSUGGEST_USE_ASYNC=1  # speed up autosuggestions
