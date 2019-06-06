ca09d20895bf79d1775592030a3056f58e12a073
Ryan Johnston <ryjo1026@gmail.com>
2019-06-05 23:47:05 -0400

    Add some useful aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,9 @@
 
 alias "newtab"='open . -a iterm'                        # Open pwd in new iTerm tab
 alias "kraken"='open -na "GitKraken" --args -p $(pwd)'  # Open pwd in GitKraken
+alias "chrome"='open -a "Google Chrome"'                # Open file in Chrome (best used with .html)
+
+alias "zshrc"='source ~/.zshrc'                         # Reload the shell with preferences
 
 alias "grep"='grep --color'                             # Make grep colorful
 alias "ccat"='pygmentize'                               # Make ccat colorful
