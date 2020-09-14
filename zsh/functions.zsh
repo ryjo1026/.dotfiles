9d666d209ea18b58fff1dfb08b77abc94e5191f8
Ryan Johnston <ryjo1026@gmail.com>
2020-09-14 13:32:20 -0400

    Add ls-tap function
    Should list the tap's ($1 arg) available formulae

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -8,3 +8,9 @@ function mkcd() {
 function gitignore() {
     echo $1 >> .gitignore
 }
+
+function ls-tap() {
+  TAP_PREFIX=$(brew --prefix)/Homebrew/Library/Taps; \
+  ls $TAP_PREFIX/$1/Formula/*.rb 2>/dev/null || ls $TAP_PREFIX/$1/*.rb 2>/dev/null | \
+  xargs -I{} basename {} .rbI
+}
